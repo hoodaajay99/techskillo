9be6ad7fbef1762a8fc4594f674cf66fdf7dd76f
Ajay Hooda <hoodaajay@gmail.com>
2019-12-31 22:50:22 +0530

    home and other images

--- src/app/courses/courses.page.ts
+++ src/app/courses/courses.page.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesPage implements OnInit {
 
+  coursesList:any [] = [
+    {
+      imgsrc: "assets/imgs/prog-c-lang.png",
+      title: "Programming in C Language",
+      shortDesc: "Learn C Programming from Beginners to Advanced Course. This tutorial does not require any prior programming experience.",
+      link:"https://cprog.techskillo.com"
+    },
+    {
+      imgsrc: "assets/imgs/prog-linux-device-drivers.png",
+      title: "Linux Kernel and Device Drivers",
+      shortDesc: "Learn Linux Kernel Internals and Device Drivers, including Kernel Modules, Character Drivers, Network Drivers",
+      link:"https://ldd.techskillo.com"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,38 @@ import { Platform } from '@ionic/angular';
 })
 export class HomePage {
 
+
+  cardsList:any [] = [
+    {
+      imgsrc: "assets/imgs/prog-c-lang.png",
+      title: "Online Courses",
+      shortDesc: "Library of online courses, comes with open source content",
+      link:"https://cprog.techskillo.com",
+      buttonText: "Try Now"
+    },
+    {
+      imgsrc: "assets/imgs/prog-c-lang.png",
+      title: "Video Courses",
+      shortDesc: "Learn from library of engaging video build with audio video tools",
+      link:"https://cprog.techskillo.com",
+      buttonText: "Try Now"
+    },
+    {
+      imgsrc: "assets/imgs/prog-linux-device-drivers.png",
+      title: "Offline Bootcamps",
+      shortDesc: "Learn from our open source course content, practice at your own pace ",
+      link:"https://ldd.techskillo.com",
+      buttonText: "Try Now"
+    },
+    {
+      imgsrc: "assets/imgs/prog-linux-device-drivers.png",
+      title: "Coding Challenges",
+      shortDesc: "Test your skills with out state of the art Coding Challenges",
+      link:"https://ldd.techskillo.com",
+      buttonText: "Try Now"
+    }    
+  ];
+
   constructor(private platform: Platform) {
     
   }
