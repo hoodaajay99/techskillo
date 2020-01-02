30ee2c86907860449cc00064ebfd19a7c2247fea
Ajay Hooda <hoodaajay@gmail.com>
2020-01-02 13:18:32 +0530

    home page and images update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,28 +11,28 @@ export class HomePage {
 
   cardsList:any [] = [
     {
-      imgsrc: "assets/imgs/prog-c-lang.png",
+      imgsrc: "assets/imgs/card-online.png",
       title: "Online Courses",
       shortDesc: "Library of online courses, comes with open source content",
       link:"https://cprog.techskillo.com",
       buttonText: "Try Now"
     },
     {
-      imgsrc: "assets/imgs/prog-c-lang.png",
+      imgsrc: "assets/imgs/card-video.png",
       title: "Video Courses",
       shortDesc: "Learn from library of engaging video build with audio video tools",
       link:"https://cprog.techskillo.com",
       buttonText: "Try Now"
     },
     {
-      imgsrc: "assets/imgs/prog-linux-device-drivers.png",
+      imgsrc: "assets/imgs/card-boot.png",
       title: "Offline Bootcamps",
       shortDesc: "Learn from our open source course content, practice at your own pace ",
       link:"https://ldd.techskillo.com",
       buttonText: "Try Now"
     },
     {
-      imgsrc: "assets/imgs/prog-linux-device-drivers.png",
+      imgsrc: "assets/imgs/card-code.png",
       title: "Coding Challenges",
       shortDesc: "Test your skills with out state of the art Coding Challenges",
       link:"https://ldd.techskillo.com",
